1724f5fb90c2655a018ed044cd490eb31ea216cb
Sadeepa Chamudith <hmwchamudith@gmail.com>
2024-09-05 22:07:21 +0530

    updt

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ HEROKU_API_KEY: process.env.HEROKU_API_KEY === undefined ? 'onlygroup' : process
 HEROKU_APP_NAME: process.env.HEROKU_APP_NAME === undefined ? 'CYBER-X' : process.env.HEROKU_APP_NAME,
 ANTI_LINK: process.env.ANTI_LINK === undefined ? 'false' : process.env.ANTI_LINK,
 AUTO_REACT: process.env.AUTO_REACT === undefined ? 'true' : process.env.AUTO_REACT,
-PREFIX: process.env.PREFIX === undefined ? '> ' : process.env.PREFIX ,
+PREFIX: process.env.PREFIX === undefined ? '>' : process.env.PREFIX ,
 AI_MODE: process.env.AI_MODE === undefined ? 'true' : process.env.AI_MODE,
 BOT_DETECT: process.env.BOT_DETECT === undefined ? 'false' : process.env.BOT_DETECT,
 ANTI_BOT: process.env.ANTI_BOT === undefined ? 'false' : process.env.ANTI_BOT ,
